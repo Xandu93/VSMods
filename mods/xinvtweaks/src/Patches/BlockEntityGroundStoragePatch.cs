95f94aa69ab75904a26ebdfc7078360499f61916
Xandu <thomas.otto4@hotmail.com>
2025-03-11 15:58:01 +0100

    inv tweaks: -if you interact with groundstorage and have already a ground storable item in your active hotbar slot the mod will no longer switch items

--- mods/xinvtweaks/src/Patches/BlockEntityGroundStoragePatch.cs
+++ mods/xinvtweaks/src/Patches/BlockEntityGroundStoragePatch.cs
@@ -8,6 +8,7 @@ namespace XInvTweaks
         static public void OnPlayerInteractStartPrefix(BlockEntityGroundStorage __instance, IPlayer player)
         {
             if (__instance.Api.Side == EnumAppSide.Server) return;
+            if (player.InventoryManager.ActiveHotbarSlot.Itemstack?.Collectible.HasBehavior<CollectibleBehaviorGroundStorable>() ?? false) return;
             InventoryUtil.FindPushableCollectible(__instance.Inventory, player);
         }
     }
